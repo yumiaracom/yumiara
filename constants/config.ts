e67652d788d0fadfb0338b1a347e08f0d480baaf
lou1s vuong <lou1svuong.dev@gmail.com>
2025-01-15 17:48:24 +0700

    Refactor environment variables for wallet and smart contract addresses
    - Updated `.env.example` to rename wallet and smart contract address variables to include the `NEXT_PUBLIC_` prefix for better compatibility with client-side usage.
    - Modified `constants/config.ts` to reflect the changes in environment variable names, ensuring proper type handling for wallet and smart contract addresses.

--- constants/config.ts
+++ constants/config.ts
@@ -9,6 +9,7 @@ export const envConfig = {
   DB_PASSWORD: process.env.DB_PASSWORD as string,
   DB_NAME: process.env.DB_NAME as string,
   DB_MESSAGES_COLLECTION: process.env.DB_MESSAGES_COLLECTION as string,
-  WALLET_POOL_ADDRESS: process.env.WALLET_POOL_ADDRESS as `0x${string}`,
-  SC_ADDRESS: process.env.SC_ADDRESS as `0x${string}`,
+  WALLET_POOL_ADDRESS: process.env
+    .NEXT_PUBLIC_WALLET_POOL_ADDRESS as `0x${string}`,
+  SC_ADDRESS: process.env.NEXT_PUBLIC_SC_ADDRESS as `0x${string}`,
 } as const;
